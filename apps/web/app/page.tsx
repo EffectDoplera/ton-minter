d4517bcea2c84e2849cd1fb02f292b3a83f757f8
Danshin Mikhail <effectdopler0@gmail.com>
2024-07-29 17:40:16 +0300

    feat: integrate nextjs with blueprint

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,8 +1,36 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
+import { SimpleCounter } from '@repo/contract/SimpleCounter'
+import { getHttpV4Endpoint } from "@orbs-network/ton-access";
+import { Address, TonClient4 } from "@ton/ton";
 
-export default function Home() {
+const counterAddress = 'EQApvaV6zWC3U5lkHogHMWORX4_AlMJdeK9I_i93MSbs4uzV'
+
+export default async function Home() {
+  const counterValue = await getHttpV4Endpoint({ network: 'testnet' })
+      .then((endpoint) => {
+        console.log(endpoint)
+        return new TonClient4({ endpoint })
+      })
+      .then((client) => {
+        return client.open(SimpleCounter.fromAddress(Address.parse(counterAddress)))
+      })
+      .then(async (contract) => {
+        return {
+          value: await contract.getCounter(),
+          id: await contract.getId()
+        }
+      })
+      .then((data) => {
+        return {
+          value: data.value.toString(),
+          id: data.id.toString()
+        } 
+      })
+      .catch(() => {
+        return null
+      })
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -14,86 +42,21 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className="text-xl flex flex-row items-center gap-2 ">
+          <p className="font-bold flex-shrink-0">Address of counter:</p>
+          <p className="truncate min-w-content">{counterAddress}</p>
         </div>
-        <Button appName="web" className={styles.secondary}>
-          Open alert
-        </Button>
+
+        <div className="text-xl flex flex-row items-center gap-2 ">
+          <p className="font-bold flex-shrink-0">Counter ID:</p>
+          <p className="truncate min-w-content">{counterValue?.id ?? '0'}</p>
+        </div>
+
+        <div className="text-xl flex flex-row items-center gap-2 ">
+          <p className="font-bold flex-shrink-0">Counter value:</p>
+          <p className="truncate min-w-content">{counterValue?.value ?? '0'}</p>
+        </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file-text.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }

e94dcd480e8ef2c19aa31176ed83849ef1213ce8
Danshin Mikhail <effectdopler0@gmail.com>
2024-08-02 12:19:10 +0300

    feat: add JettonWallet contract

---
+++ packages/contract/scripts/incrementSimpleCounter.ts
@@ -0,0 +1,45 @@
+import { NetworkProvider, sleep } from '@ton/blueprint'
+import { Address, toNano } from '@ton/core'
+import { SimpleCounter } from '../wrappers/SimpleCounter'
+
+export async function run(provider: NetworkProvider, args: string[]) {
+  const ui = provider.ui()
+
+  //@ts-ignore
+  const address = Address.parse(args.length > 0 ? args[0] : await ui.input('SimpleCounter address'))
+
+  if (!(await provider.isContractDeployed(address))) {
+    ui.write(`Error: Contract at address ${address} is not deployed!`)
+    return
+  }
+
+  const simpleCounter = provider.open(SimpleCounter.fromAddress(address))
+
+  const counterBefore = await simpleCounter.getCounter()
+
+  await simpleCounter.send(
+    provider.sender(),
+    {
+      value: toNano('0.05'),
+    },
+    {
+      $$type: 'Add',
+      queryId: 0n,
+      amount: 1n,
+    },
+  )
+
+  ui.write('Waiting for counter to increase...')
+
+  let counterAfter = await simpleCounter.getCounter()
+  let attempt = 1
+  while (counterAfter === counterBefore) {
+    ui.setActionPrompt(`Attempt ${attempt}`)
+    await sleep(2000)
+    counterAfter = await simpleCounter.getCounter()
+    attempt++
+  }
+
+  ui.clearActionPrompt()
+  ui.write('Counter increased successfully!')
+}

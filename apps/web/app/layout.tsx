713da58d80f06206fc92f80c59479fb751ecdea0
Danshin Mikhail <effectdopler0@gmail.com>
2024-08-13 16:59:15 +0300

    feat: add footer

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -38,7 +38,29 @@ export default function RootLayout({
             <TonConnectButton />
           </header>
           {children}
-          <footer className="p-4 bg-slate-200">Footer</footer>
+          <footer className="py-4 px-4">
+            <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
+              Built by{' '}
+              <a
+                href="https://github.com/EffectDoplera"
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                EffectDoplera
+              </a>
+              . The source code is available on{' '}
+              <a
+                href="https://github.com/EffectDoplera/#"
+                target="_blank"
+                rel="noreferrer"
+                className="font-medium underline underline-offset-4"
+              >
+                GitHub
+              </a>
+              .
+            </p>
+          </footer>
           <Toaster />
         </Providers>
       </body>

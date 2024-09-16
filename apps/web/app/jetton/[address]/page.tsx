f4962d0763c9f790d222ca037391d55dbd0aca62
Danshin Mikhail <effectdopler0@gmail.com>
2024-09-16 16:24:38 +0300

    feat: add breadcrumbs

--- apps/web/app/jetton/[address]/page.tsx
+++ apps/web/app/jetton/[address]/page.tsx
@@ -1,5 +1,13 @@
 import { JettonPreview } from '@/entities/jetton'
 import { db } from '@/shared/database'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '@/shared/ui/breadcrumb'
 import { Card } from '@/shared/ui/card'
 import { notFound } from 'next/navigation'
 import { cache } from 'react'
@@ -19,6 +27,18 @@ const JettonPage = async ({ params }: { params: { address: string } }) => {
   const data = await getJettonById(params.address)
   return (
     <div className="grid grid-cols-1 md:grid-cols-12 gap-4 justify-center">
+      <Breadcrumb className="col-span-full">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Jetton</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
       <JettonPreview jetton={data} />
       <Card className="h-96 grid place-content-center md:col-span-8">CHART</Card>
       <Card className="h-96 grid place-content-center md:col-span-4">TRADE</Card>

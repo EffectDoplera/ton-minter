f52019a462291eb2f8ac159a29bbf55b762e8b6b
Danshin Mikhail <effectdopler0@gmail.com>
2024-09-16 16:15:49 +0300

    feat: add chart and trade mocs

--- apps/web/app/jetton/[address]/page.tsx
+++ apps/web/app/jetton/[address]/page.tsx
@@ -1,5 +1,6 @@
 import { JettonPreview } from '@/entities/jetton'
 import { db } from '@/shared/database'
+import { Card } from '@/shared/ui/card'
 import { notFound } from 'next/navigation'
 import { cache } from 'react'
 
@@ -17,9 +18,11 @@ const getJettonById = cache(async (id: string) => {
 const JettonPage = async ({ params }: { params: { address: string } }) => {
   const data = await getJettonById(params.address)
   return (
-    <>
+    <div className="grid grid-cols-1 md:grid-cols-12 gap-4 justify-center">
       <JettonPreview jetton={data} />
-    </>
+      <Card className="h-96 grid place-content-center md:col-span-8">CHART</Card>
+      <Card className="h-96 grid place-content-center md:col-span-4">TRADE</Card>
+    </div>
   )
 }
 
